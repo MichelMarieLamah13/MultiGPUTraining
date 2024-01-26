9495dbb3d4a65eecbc25be4cc8a078501506d2c3
miguel <lamahmichelmarie@gmail.com>
2024-01-26 16:50:22 +0100

    26 Janv 2024 16:50

--- distributed_data_parallel.sh
+++ distributed_data_parallel.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=mgpu_3
-#SBATCH --nodes=2
+# #SBATCH --nodes=2
 #SBATCH --partition=gpu
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:2
 #SBATCH --constraint=GPURAM_Max_12GB
 #SBATCH --time=7-00:00:00
 #SBATCH --mem=16GB
